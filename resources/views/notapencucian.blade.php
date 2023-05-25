6f0c92861e9fac7261afbaae6ec466cf50570ce4
Vicky Valerian <vickyvalerian27@gmail.com>
2023-05-25 21:03:28 +0700

    invoie -> nota transaksi pencucian

--- resources/views/notapencucian.blade.php
+++ resources/views/notapencucian.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<title>Invoice</title>
+		<title>Nota Transaksi Pencucian</title>
 		<link rel="license" href="https://www.opensource.org/licenses/mit-license/">
         <style>
             /* reset */
